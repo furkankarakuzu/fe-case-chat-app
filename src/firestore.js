6d08f300ff8fe93e3f35c21f970515e2f869d9c4
Furkan Karakuzu <46384767+furkankarakuzu@users.noreply.github.com>
2021-03-17 21:41:10 +0300

    Update firestore.js

--- src/firestore.js
+++ src/firestore.js
@@ -1,12 +1,6 @@
 import firebase from 'firebase'
 const config = {
-  apiKey: "AIzaSyAs4fKMCwVWF-_mrXqTz6LIDlNZCvSecqY",
-  authDomain: "fe-case.firebaseapp.com",
-  databaseURL: "https://fe-case-default-rtdb.firebaseio.com",
-  projectId: "fe-case",
-  storageBucket: "fe-case.appspot.com",
-  messagingSenderId: "66719958169",
-  appId: "1:66719958169:web:2c8ab0c935a89a8f7ee408"
+//YOUR CONFIG
 };
 
 let fire = firebase.initializeApp(config);
